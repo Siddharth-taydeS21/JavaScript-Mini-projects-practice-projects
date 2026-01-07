f4ba77df634126c8b223518d95a953717cb5c699
Siddharth Tyade <taydesiddharth999@gmail.com>
2026-01-07 19:26:30 +0530

    Minimal Commit

---
+++ mini_project_OOP-JS/index.js
@@ -0,0 +1,5 @@
+// Make the create pensile constructor with write and erase method;
+
+// Do Some Practice on Class, Constructor, Extend, super
+
+// learn more about prototypal inhertance & practice on Object.create() & Object.prototype
